554bc3c3be9596d8d7ef5020a04facc382f287f5
Dave Myers <contact@ameravant.com>
2011-12-05 14:43:54 -0800

    adding logic to tag_cloud for accounts

--- lib/domainify/konstants.rb
+++ lib/domainify/konstants.rb
@@ -3,6 +3,6 @@ module Konstants
                    event_price_options event_registrations event_transactions events featurable_sections
                    features folders galleries images inquiries link_categories links menus newsletter_blasts 
                    newsletters pages people person_groups product_categories product_options products 
-                   profiles redirects searches settings taggings tags templates testimonials users)
+                   profiles redirects searches settings taggings templates testimonials users)
   Klasses = TableNames.reject{|t| !ActiveRecord::Base.connection.tables.include?(t)}.collect{|c| c.camelcase.singularize.constantize}
 end
