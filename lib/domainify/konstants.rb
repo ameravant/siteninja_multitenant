28572186207ea848fcbd50d8dace18e6b848b39d
Dave Myers <contact@ameravant.com>
2012-02-01 11:47:09 -0800

    adding feeds to konstants

--- lib/domainify/konstants.rb
+++ lib/domainify/konstants.rb
@@ -1,7 +1,7 @@
 module Konstants
   TableNames = %w(article_categories articles assets column_sections columns comments emails event_categories 
                    event_price_options event_registrations event_transactions events featurable_sections
-                   features folders galleries images inquiries link_categories links menus newsletter_blasts 
+                   features feeds folders galleries images inquiries link_categories links menus newsletter_blasts 
                    newsletters pages people person_groups product_categories product_options products 
                    profiles redirects searches settings taggings templates testimonials users)
   Klasses = TableNames.reject{|t| !ActiveRecord::Base.connection.tables.include?(t)}.collect{|c| c.camelcase.singularize.constantize}
