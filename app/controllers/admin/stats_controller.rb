2aa96bfd00a5e767609b0a192f5c9ee5eed5d3ec
Dave Myers <contact@ameravant.com>
2012-01-31 16:40:49 -0800

    formatting stats

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -8,7 +8,7 @@ class Admin::StatsController < AdminController
     if params[:remote_ip]
       add_breadcrumb "Stats", admin_account_stats_path(@account.id)
       add_breadcrumb params[:remote_ip]
-      stats = Stat.all(:order => "created_at DESC", :conditions => ['account_id = ? and created_at > ? and remote_ip = ?', @account.id, Time.now.beginning_of_month, params[:remote_ip]])
+      stats = Stat.all(:conditions => ['account_id = ? and created_at > ? and remote_ip = ?', @account.id, Time.now.beginning_of_month, params[:remote_ip]])
     else
       add_breadcrumb "Stats"
       stats = Stat.all(:order => "created_at DESC", :conditions => ['account_id = ? and created_at > ?', @account.id, Time.now.beginning_of_month])
